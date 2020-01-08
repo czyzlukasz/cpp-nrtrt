833f7d5da5d166165a9b595f972d88425419c10f
krawacik3 <krawacik3@gmail.com>
2020-01-08 19:50:35 +0100

    Fixed Transparent objects

--- Src/Object/TransparentSphere.hpp
+++ Src/Object/TransparentSphere.hpp
@@ -14,22 +14,28 @@ struct TransparentSphere : public Sphere{
         {}
 
     [[nodiscard]] Ray outputRay(const glm::vec3 sphereCollisionPoint, const glm::vec3& direction) const{
+        const Ray internalRay = calculateInternalRay(sphereCollisionPoint, direction);
+        return internalToOutputRay(internalRay);
+    }
+
+    //TODO: for UTs
+//private:
+    [[nodiscard]] inline Ray calculateInternalRay(const glm::vec3 sphereCollisionPoint, const glm::vec3& direction) const{
         //Calculate angle between normal and direction
         const glm::vec3 tempNormal = normalAtPoint(sphereCollisionPoint);
         const glm::vec3 normal = glm::dot(tempNormal, direction) < 0.f ? tempNormal : - tempNormal;
         const float angle = glm::angle(-normal, direction);
-        
+
         //Calculate internal ray
         const glm::vec3 crossProduct = glm::cross(direction, normal);
         const float sinAngle = std::sin(angle);
         const float internalAngle = std::asin(1.f / indexOfRefraction * sinAngle);
         const glm::vec3 internalRayDirection = glm::normalize(glm::rotate(-normal, internalAngle, crossProduct));
         const Ray internalRay{sphereCollisionPoint, internalRayDirection};
-        return internalToOutputRay(internalRay);
+        return internalRay;
     }
 
-private:
-    [[nodiscard]] Ray internalToOutputRay(const Ray& ray) const{
+    [[nodiscard]] inline Ray internalToOutputRay(const Ray& ray) const{
         const glm::vec3 sphereCollisionPoint = collisionPoint(ray);
         const glm::vec3 tempNormal = normalAtPoint(sphereCollisionPoint);
         const glm::vec3 normal = glm::dot(ray.direction, tempNormal) > 0.f ? tempNormal : - tempNormal;
@@ -37,19 +43,19 @@ private:
 
         const glm::vec3 crossProduct = glm::cross(ray.direction, normal);
         if(isTotalInternalReflection(angle)){
-            const float outputAngle = std::asin(indexOfRefraction * std::sin(angle) - M_PI_2);
-            const glm::vec3 outputRayDirection = glm::normalize(glm::rotate(normal, outputAngle, crossProduct));
+            const float outputAngle = -std::asin(indexOfRefraction * std::sin(angle) - M_PI_2);
+            const glm::vec3 outputRayDirection = glm::normalize(glm::rotate(-normal, outputAngle, crossProduct));
             return internalToOutputRay(Ray{sphereCollisionPoint, outputRayDirection});
         }
         else{
-            const float outputAngle = std::asin(indexOfRefraction * std::sin(angle));
+            const float outputAngle = -std::asin(indexOfRefraction * std::sin(angle));
             const glm::vec3 outputRayDirection = glm::normalize(glm::rotate(normal, outputAngle, crossProduct));
             return Ray{sphereCollisionPoint, outputRayDirection};
         }
     }
 
     inline bool isTotalInternalReflection(float angle) const{
-        return std::asin(1.f / indexOfRefraction) <= angle;
+        return std::asin(indexOfRefraction) <= angle;
     }
 
     const float indexOfRefraction;
--- Uts/TransparentSphereTest.cpp
+++ Uts/TransparentSphereTest.cpp
@@ -7,7 +7,7 @@
 
 
 struct TransparentSphereTest : public ::testing::Test{
-    TransparentSphereTest() : tSphere(glm::vec3(3,3,3), 2.f, Pixel<>{255,255,255,255}, 2.f){}
+    TransparentSphereTest() : tSphere(glm::vec3(3,3,3), 2.f, Pixel<>{255,255,255,255}, 1.5f){}
 
 protected:
     TransparentSphere tSphere;
@@ -19,16 +19,18 @@ TEST_F(TransparentSphereTest, CheckCenterRay){
     const glm::vec3 collisionPoint = tSphere.collisionPoint(centerRay);
     const Ray result = tSphere.outputRay(collisionPoint, centerRay.direction);
     EXPECT_FLOAT_EQ(result.direction.x, centerRay.direction.x);
-    EXPECT_NEAR(result.direction.y, centerRay.direction.y, 0.0001);
+    EXPECT_NEAR(result.direction.y, centerRay.direction.y, 0.0002);
     EXPECT_FLOAT_EQ(result.direction.z, centerRay.direction.z);
 }
 
 TEST_F(TransparentSphereTest, CheckRayAt45Degree){
     //Hit at 45 degrees
-    const Ray centerRay{{3,3 + 2. * std::sqrt(2.f),0}, {0,0,1}};
+    const Ray centerRay{{3,3 + 2.f / std::sqrt(2.f),0}, {0,0,1}};
     const glm::vec3 collisionPoint = tSphere.collisionPoint(centerRay);
-    EXPECT_FLOAT_EQ(glm::dot(centerRay.direction, tSphere.normalAtPoint(collisionPoint)), M_PI_4);
-    const Ray result = tSphere.outputRay(collisionPoint, centerRay.direction);
-    const float internalAngle = std::asin(std::sin(M_PI_4) * 0.5f);
-// 
+    //Check if ray hits at 45 degrees
+    EXPECT_NEAR(glm::dot(-centerRay.direction, tSphere.normalAtPoint(collisionPoint)), std::sin(M_PI_4), 1e-5);
+    const Ray result = tSphere.calculateInternalRay(collisionPoint, centerRay.direction);
+    const float internalAngleCalculated = std::asin(std::sin(M_PI_4) * 1.f / 1.5f);
+    const float internalAngleResult = glm::angle(result.direction, -tSphere.normalAtPoint(collisionPoint));
+    EXPECT_NEAR(internalAngleResult, internalAngleCalculated, 1e-5);
 }
